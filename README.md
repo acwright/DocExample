5616280deb7c540c5ec88d6c9f99397e4e98f972
Aaron Wright <aaron.c.wright@icloud.com>
2020-10-01 12:41:17 -0500

    Update for Xcode 12.2

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ DocExample
 An example of using `ReferenceFileDocument` in SwiftUI.
 
 Read the full blog post [here](https://medium.com/@acwrightdesign/using-referencefiledocument-in-swiftui-e54ef75a14b8).
+
+Note: Xcode 12.2 or higher is needed!
--- Shared/DocExampleDocument.swift
+++ Shared/DocExampleDocument.swift
@@ -25,19 +25,6 @@ class DocExampleDocument: ReferenceFileDocument {
     }
 
     static var readableContentTypes: [UTType] { [.exampleText] }
-
-    #if os(macOS)
-    
-    required init(fileWrapper: FileWrapper, contentType: UTType) throws {
-        guard let data = fileWrapper.regularFileContents,
-              let string = String(data: data, encoding: .utf8)
-        else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-        text = string
-    }
-    
-    #elseif os(iOS)
     
     required init(configuration: ReadConfiguration) throws {
         guard let data = configuration.file.regularFileContents,
@@ -48,24 +35,11 @@ class DocExampleDocument: ReferenceFileDocument {
         text = string
     }
     
-    #endif
-    
-    #if os(macOS)
-    
-    func write(snapshot: String, to fileWrapper: inout FileWrapper, contentType: UTType) throws {
-        let data = snapshot.data(using: .utf8)!
-        fileWrapper = FileWrapper(regularFileWithContents: data)
-    }
-    
-    #elseif os(iOS)
-    
     func fileWrapper(snapshot: String, configuration: WriteConfiguration) throws -> FileWrapper {
         let data = snapshot.data(using: .utf8)!
         
         return FileWrapper(regularFileWithContents: data)
-    }
-    #endif
-    
+    }    
     func snapshot(contentType: UTType) throws -> String {
         return self.text
     }
